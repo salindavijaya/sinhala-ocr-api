8399703874665e6bed4bf64e7a0076397112e3ee
salindavijaya <85182531+salindavijaya@users.noreply.github.com>
2026-05-10 21:42:47 +0530

    Update GCP keyFile configuration logic

--- src/config/index.js
+++ src/config/index.js
@@ -54,7 +54,7 @@ const config = {
   },
   gcp: {
     projectId: process.env.GCP_PROJECT_ID || '',
-    keyFile: process.env.GCP_SA_KEY || JSON.stringify(GCP_KEY),
+    keyFile: process.env.GCP_KEY_FILE || process.env.GCP_SA_KEY || JSON.stringify(GCP_KEY),
     storage: {
       inputBucket: process.env.GCS_BUCKET_NAME || 'sinhala-ocr-uploads',
       outputBucket: 'sicript_bucket_output' || process.env.GCS_OUTPUT_BUCKET_NAME || 'sinhala-ocr-outputs',
